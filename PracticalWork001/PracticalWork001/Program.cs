e29f354b6b174fb832f58cddf8fb7fc907cde393
DmitriyKuznecov-azur <kuznecov.d@azurgames.com>
2023-07-29 17:20:20 +0300

    added solution

---
+++ PracticalWork001/PracticalWork001/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PracticalWork001
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Задание 1
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+            
+            // Задание 2
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write(" !!! ");
+            Console.ReadKey();
+        }
+    }
+}
+
