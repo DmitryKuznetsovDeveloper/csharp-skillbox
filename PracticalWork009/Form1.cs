19b986c9a1f17bf04b136657d93b4d7f05bdca10
DmitriyKuznecov-azur <kuznecov.d@azurgames.com>
2023-08-08 07:04:25 +0300

    added solution

---
+++ PracticalWork009/Form1.cs
@@ -0,0 +1,33 @@
+using System.Collections.ObjectModel;
+
+namespace PracticalWork009
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Splite_Click(object sender, EventArgs e)
+        {
+            string[] array = textBox1.Text.Split();
+            for (int i = 0; i < array.Length; i++)
+            {
+                listBox1.Items.Add(array[i]);
+            }
+        }
+
+        private void MyReverse_Click(object sender, EventArgs e)
+        {
+            string[] array = textBox2.Text.Split();
+            Array.Reverse(array);
+            label2.Text = string.Join(" ", array);
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
